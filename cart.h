4c04587f93e0c72453af3a23b013260dcf195852
EvanC545 <86642032+EvanC545@users.noreply.github.com>
2022-11-26 20:42:45 -0600

    Update cart.h

--- cart.h
+++ cart.h
@@ -4,12 +4,25 @@
 using std::string;
 
 class Cart {
+	
 private:
+	
 	string item;
 	double itemPrice;
 	int numItems;
 	double totalCost;
+	
 public:
+	
+	// Constructors
+	Cart();
+	Cart(string c_item, double price, int numItems, double c_cost);
+	//	1		2		3		4
+	// 1) Array of items in cart
+	// 2) Array of prices of items in cart
+	// 3) Number of items in cart
+	// 4) Sum of all prices in price array
+	
 	string viewCart(string item);
 	bool cartIsEmpty();
 	void addItem(string item);
