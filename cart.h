262c393d359094d8d0adefec82dc17c2a85a50aa
EvanC545 <86642032+EvanC545@users.noreply.github.com>
2022-11-26 22:44:18 -0600

    Update cart.h

--- cart.h
+++ cart.h
@@ -1,33 +1,45 @@
 #pragma once
 #include <string>
+#include <vector>
 
 using std::string;
 
 class Cart {
 	
 private:
+	string tempItem;
+	vector<string> item;
+	vector<double> itemPrice;
+	//int numItems;
+	//double totalCost;
+	//double subTotal;
 	
-	string item;
-	double itemPrice;
-	int numItems;
-	double totalCost;
+	//Regular functions
+	void viewCart(vector<string> item, vector<double> itemPrice);
+	bool cartIsEmpty(vector<string> item);
+	void addItem(vector<string> item, vector<double> itemPrice, string tempItem);
+	void decItem(vector<string> item, vector<double> itemPrice, string tempItem);
+	void removeItem(vector<string> item, vector<double> itemPrice, string tempItem);
+	void checkout(vector<string> item, vector<double> itemPrice);
+	double calcSubtotal(vector<double> itemPrice);
 	
 public:
 	
 	// Constructors
 	Cart();
-	Cart(string c_item, double price, int numItems, double c_cost);
-	//	1		2		3		4
-	// 1) Array of items in cart
-	// 2) Array of prices of items in cart
-	// 3) Number of items in cart
-	// 4) Sum of all prices in price array
+	//Cart(string c_item, double price); // int numItems, double c_cost
+	//	   1		2		3		4
+	// 1) Vector of items in cart
+	// 2) Vector of prices of items in cart
+	// 3) Number of items in cart (unused)
+	// 4) Sum of all prices in price array (unused)
 	
-	string viewCart(string item);
+	// Public facing functions
+	void viewCart();
 	bool cartIsEmpty();
-	void addItem(string item);
-	void decItem(string item, bool cartIsEmpty);
-	void removeItem(string item);
-	double calcSubtotal(double itemPrice);
-	void checkout(string item, double calcSubtotal);
+	void addItem(string tempItem);
+	void decItem(string tempItem);
+	void removeItem(string tempItem);
+	void checkout();
+	double calcSubtotal();
 };
