fe88f2cdfb14fbda487a61c4d856d7183d52d83e
EvanC545 <86642032+EvanC545@users.noreply.github.com>
2022-11-30 04:57:37 -0600

    More Homework 5 files

---
+++ Homework 5/simple_test.py
@@ -0,0 +1,60 @@
+import pytest
+import math
+## functions to be tested
+def openFile(filename):
+    infile = open(filename, "r")
+
+    print("File opened.")
+
+def numbers(num1, num2):
+    return num1 / num2
+
+def dist(x1, y1, x2, y2):
+    dist = (x2 - x1) ** 2 + (y2 - y1) ** 2
+    dist = math.sqrt(dist)
+
+    return dist
+
+def isPalindrome(temp):
+    test = temp[::-1]
+
+    if(test == temp):
+        return True
+
+    else:
+        return False
+
+def divide():
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter another number: "))
+
+    div = num1 / num2
+
+    print("Your numbers divided is:", div)
+
+def sq(num):
+    return math.sqrt(num)
+
+def greetUser(first, middle, last):
+    print("Hello!")
+    print("Welcome to the program", first, middle, last)
+    print("Glad to have you!")
+
+def displayItem(numbers, index):
+    print("Your item at", index, "index is", numbers[index])
+
+## functions to test functions
+
+@pytest.mark.parametrize("filename", [1, "Stuff", "%$#"])
+def test_openFile(filename):
+    assert openFile(filename) == "File opened."
+
+@pytest.mark.parametrize("num1, num2", [(4, 0), ("num1", 2), (True, False)])
+def test_numbers(num1, num2):
+    assert numbers(num1, num2) == 1
+
+@pytest.mark.parametrize("x1, y1, x2, y2", [(0, 0, math.sqrt(7), math.sqrt(2)), ("x1", "y1", "x2", "y2"), (True, False, 3, 4)])
+def test_dist(x1, y1, x2, y2):
+    assert dist(x1, y1, x2, y2) == 3 
+    ##first test is supposed to equal 3, but instead python says it's equal to 3.0000000000000004
+    ##third test returns an actual number too, somehow.
