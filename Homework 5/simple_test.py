9b3cfb5e9bbf46b4ccc8d2e68c613cb0e49cd45b
EvanC545 <86642032+EvanC545@users.noreply.github.com>
2022-11-30 23:46:13 -0600

    Add files via upload

--- Homework 5/simple_test.py
+++ Homework 5/simple_test.py
@@ -53,7 +53,8 @@ def test_openFile(filename):
 def test_numbers(num1, num2):
     assert numbers(num1, num2) == 1
 
-@pytest.mark.parametrize("x1, y1, x2, y2", [(0, 0, math.sqrt(7), math.sqrt(2)), ("x1", "y1", "x2", "y2"), (0, 0, 3, 4)])
+
+@pytest.mark.parametrize("x1, y1, x2, y2", [(0, 0, math.sqrt(7), math.sqrt(2)), ("x1", "y1", "x2", "y2"), (True, False, 3, 4)])
 def test_dist(x1, y1, x2, y2):
     assert dist(x1, y1, x2, y2) == 3 
     ##first test is supposed to equal 3, but instead python says it's equal to 3.0000000000000004
@@ -64,3 +65,19 @@ def test_isPalindrome(temp):
     assert isPalindrome(temp) == True
     ##first test succeeds
     ##second and third tests obviously fail because incorrect object
+
+def test_divide(monkeypatch, capsys):
+    divide()
+    monkeypatch.setattr('builtins.input', lambda _: "8")
+    #monkeypatch.setattr('builtins.input', lambda _: 4)
+    
+    captured_stdout, captured_stderr = capsys.readouterr()
+    assert captured_stdout.strip() == "Your numbers divided is: 2"
+    #Finish this function
+
+
+@pytest.mark.parametrize("num", [4, 2**2, (True+True)**2, "4"])
+def test_sq(num):
+    assert sq(num) == 2
+
+#def test_greetUser
